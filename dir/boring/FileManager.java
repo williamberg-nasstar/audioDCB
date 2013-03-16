75f0ff21599ce77176bb19be17fded6e8f50b139
William Berg <william.berg08@gmail.com>
2013-03-16 18:04:46 +0000

    stop git being a big pussy

---
+++ dir/boring/FileManager.java
@@ -0,0 +1,49 @@
+package boring;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class FileManager
+{
+  public static AudioInputStream getAudioStreamForFileInWD(String filename) throws UnsupportedAudioFileException, IOException
+  {
+    String path = System.getProperty("user.dir") + "\\" + filename;
+    
+    AudioInputStream returnAIS = null;
+    returnAIS = AudioSystem.getAudioInputStream(new File(path));
+    
+    return returnAIS;
+  }
+  
+  public static AudioInputStream getAudioStreamForExistingFile(String path) throws UnsupportedAudioFileException, IOException
+  {
+    AudioInputStream returnAIS = null;
+    returnAIS = AudioSystem.getAudioInputStream(new File(path));
+    
+    return returnAIS;
+  }
+  
+  public static File getFilehandleForNewFile(String filename)
+  {
+    File f = new File(System.getProperty("user.dir") + "\\" + filename);
+    try
+    {
+      f.createNewFile();
+    } catch (IOException e)
+    {
+      System.out.println("A weird error occurred: " + e.getMessage());
+    }
+    
+    return f;
+  }
+  
+  public static void write(AudioInputStream s, File f) throws IOException, UnsupportedAudioFileException
+  {
+    AudioSystem.write(s, AudioFileFormat.Type.WAVE, f);
+  }
+}
---
+++ dir/fun/FileUtility.java
@@ -0,0 +1,35 @@
+package fun;
+
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+
+public class FileUtility
+{
+  
+  // returns audio data
+  public static byte[] bytesFromAIS(AudioInputStream ais)
+  {
+    byte[] buffer = new byte[ais.getFormat().getFrameSize()];
+    byte[] audioBytes = new byte[(int) (ais.getFormat().getFrameSize() * ais.getFrameLength())];
+    
+    int frameOffset = 0;
+    try
+    {
+      while (ais.read(buffer) != -1)
+      {
+        for(int i = 0;i<buffer.length;i++)
+        {
+          audioBytes[(frameOffset * buffer.length)+ i] = buffer[i];
+        }
+        
+        frameOffset++;
+      }
+    } catch (IOException e)
+    {
+      System.out.println("A weird error occurred: " + e.getMessage());
+    }
+    
+    return audioBytes;
+  }
+}
